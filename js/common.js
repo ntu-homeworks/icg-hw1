aa1a2494741aa9110ebe6ccac1ac2605f574622c
Chih-Yung Liang <liangcy@cs.nctu.edu.tw>
2016-11-01 00:10:40 +0800

    Create a better interface to operate GL.

---
+++ js/common.js
@@ -0,0 +1,43 @@
+function loadTeapot() {
+    var _teapot = {
+        data: {},
+        transform: {
+            translate: [0, 0, -40],
+            rotate: {
+                angle: 0,
+                around: [0, 1, 0]
+            }
+        },
+        attributes: {
+            ambientColorUniform: [0.0, 0.0, 0.0],
+            pointLightingLocationUniform: [0.0, 0.0, 0.0],
+            pointLightingSpecularColorUniform: [0.0, 0.0, 0.0],
+            pointLightingDiffuseColorUniform: [0.0, 0.0, 0.0],
+
+            materialShininessUniform: 0.0
+        }
+    };
+
+    var request = new XMLHttpRequest();
+    request.open("GET", "objects/Teapot.json");
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            _teapot.data = JSON.parse(request.responseText);
+            gl.addObject(_teapot);
+        }
+    }
+    request.send();
+
+    return _teapot;
+}
+
+var lastTime = 0;
+function animate() {
+    var timeNow = new Date().getTime();
+    if (lastTime != 0) {
+        var elapsed = timeNow - lastTime;
+        teapotAngle += 0.03 * elapsed;
+    }
+    lastTime = timeNow;
+}
+
